18c3bf0d62b14a763885a36cfdfc8e8f75fe5fd7
Raj Shah <rajshah9305@gmail.com>
2025-09-03 01:07:18 -0600

    Delete utils directory

--- utils/sound.ts
+++
@@ -1,8 +0,0 @@
-// A simple audio player for UI sound effects.
-// Sounds have been disabled per user request.
-
-type Sound = 'deploy' | 'click' | 'complete' | 'error' | 'typing';
-
-export const playSound = async (sound: Sound, volume: number = 0.5) => {
-  // This function is intentionally left empty to disable all sounds.
-};
